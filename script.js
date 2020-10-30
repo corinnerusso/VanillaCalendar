260f39e9b1a25f64e2bf2652e8e270d24247605a
corinnerusso <corinne.russo@gmail.com>
2020-10-30 12:26:46 +0100

    change quick access into a unique function

--- script.js
+++ script.js
@@ -78,118 +78,111 @@ function setDate(){
 
 /***QUICK ACCESS ***/
 
-//today
-function isToday(){
-    let currDate = new Date();
-    let convertDate=currDate.toISOString().substr(0,10);
-    document.getElementById("endDate").value=convertDate;
-    document.getElementById("startDate").value=convertDate;
-}
-
-//the day before
-function theDayBefore(){
-    let currDate = new Date();
-    let finalDate=currDate.setDate(currDate.getDate() - 1);
-
-    let dateObject = new Date(finalDate);
-    let convertDate=dateObject.toISOString().substr(0,10);
-    document.getElementById("endDate").value=convertDate;
-    document.getElementById("startDate").value=convertDate;
-    document.getElementById("numdays").value="1";
-}
-
-// 7 last days
-function sevenLastDays(){
-    let currDate = new Date();
-    let myNewDate= new Date(currDate);
-    let finalDate=currDate.setDate(currDate.getDate() - 6);
-
-    let dateObject = new Date(finalDate);
-    let dateObject2 = new Date (myNewDate);
-
-    let convertDate=dateObject.toISOString().substr(0,10);
-    let convertDate2=dateObject2.toISOString().substr(0,10);
- 
-    document.getElementById("startDate").value=convertDate;
-    document.getElementById("endDate").value=convertDate2;
-
-    document.getElementById("numdays").value="7";
-    }
-
-// this week
-function currWeek(){
-    let currDate = new Date(document.getElementById("endDate").value);
-    // let myNewDate= new Date(currDate);   
-    day = currDate.getDay(); 
-    first_day = new Date(currDate.getTime() - 60*60*24* (day-1)*1000); 
-    last_day = new Date(first_day.getTime() + 60 * 60 *24 * 6 * 1000); 
-
-    let dateObject = new Date(first_day);
-    let convertDate=dateObject.toISOString().substr(0,10);
-    document.getElementById("startDate").value=convertDate;
-
-    let dateObject2 = new Date(last_day);
-    let convertDate2=dateObject2.toISOString().substr(0,10);
-    document.getElementById("endDate").value=convertDate2;
-
-    document.getElementById("numdays").value="7";
-    }
-
-    // 30 last days
-function thirtyLastDays(){
-    let currDate = new Date();
-    let myNewDate= new Date(currDate);   
-    let finalDate=currDate.setDate(currDate.getDate() - 29);
-
-    let dateObject = new Date(finalDate);
-    let dateObject2 = new Date (myNewDate);
-
-    let convertDate=dateObject.toISOString().substr(0,10);
-    let convertDate2=dateObject2.toISOString().substr(0,10);
-
-    document.getElementById("startDate").value=convertDate;
-    document.getElementById("endDate").value=convertDate2;
-
-    document.getElementById("numdays").value="30";
-    }
-
-
-    // this month
-function currMonth(){
-    let currDate = new Date(document.getElementById("endDate").value);
-   
-    first_day_of_month = new Date(currDate.getFullYear(),currDate.getMonth(),+1); 
-    last_day_of_month = new Date(currDate.getFullYear(),currDate.getMonth() + 1,0); 
-
-    let firstDay=first_day_of_month.setDate(first_day_of_month.getDate() + 1);
-    let LastDay=last_day_of_month.setDate(last_day_of_month.getDate() + 1);
-
-    let dateObject = new Date(firstDay);
-    let convertDate=dateObject.toISOString().substr(0,10);
-    document.getElementById("startDate").value=convertDate;
-
-    let dateObject2 = new Date(LastDay);
-    let convertDate2=dateObject2.toISOString().substr(0,10);
-    document.getElementById("endDate").value=convertDate2;
-    document.getElementById("numdays").value="30";
-    }
-
-// this year
-function currYear(){
-    let currDate = new Date(document.getElementById("endDate").value);
-           
-    first_day_of_year = new Date(currDate.getFullYear(), 0); 
-    let firstDay=first_day_of_year.setDate(first_day_of_year.getDate()+1);
-    last_day_of_year = new Date(currDate.getFullYear(), 12)  
+    $(".quick_access_link").click(function(){
+        let currDate = new Date();
+        let id = $(this).attr('id');
         
-    let dateObject = new Date(firstDay);
-    let convertDate=dateObject.toISOString().substr(0,10);
-    document.getElementById("startDate").value=convertDate;
-    
-    let dateObject2 = new Date(last_day_of_year);
-    let convertDate2=dateObject2.toISOString().substr(0,10);
-    document.getElementById("endDate").value=convertDate2;
-    document.getElementById("numdays").value="365";
-    }
+        
+        //today
+        if(id === "today"){
+            let convertDate=currDate.toISOString().substr(0,10);
+            document.getElementById("endDate").value=convertDate;
+            document.getElementById("startDate").value=convertDate;
+        } 
+        
+        //yesterday 
+            else if(id === "yesterday") {
+                let finalDate=currDate.setDate(currDate.getDate() - 1);
+                let dateObject = new Date(finalDate);
+                let convertDate=dateObject.toISOString().substr(0,10);
 
+                document.getElementById("endDate").value=convertDate;
+                document.getElementById("startDate").value=convertDate;
+                document.getElementById("numdays").value="1";
+            }
+        //last 7 days
+              else if(id === "last7days") {
 
+                let myNewDate= new Date(currDate);
+                let finalDate=currDate.setDate(currDate.getDate() - 6);
+            
+                let dateObject = new Date(finalDate);
+                let dateObject2 = new Date(myNewDate);
+            
+                let convertDate=dateObject.toISOString().substr(0,10);
+                let convertDate2=dateObject2.toISOString().substr(0,10);
+             
+                document.getElementById("startDate").value=convertDate;
+                document.getElementById("endDate").value=convertDate2;
+            
+                document.getElementById("numdays").value="7";
+            }
+        //this week 
+              else if(id === "thisweek") {
+
+                day = currDate.getDay(); 
+                first_day = new Date(currDate.getTime() - 60*60*24* (day-1)*1000); 
+                last_day = new Date(first_day.getTime() + 60 * 60 *24 * 6 * 1000); 
+            
+                let dateObject = new Date(first_day);
+                let convertDate=dateObject.toISOString().substr(0,10);
+                document.getElementById("startDate").value=convertDate;
+            
+                let dateObject2 = new Date(last_day);
+                let convertDate2=dateObject2.toISOString().substr(0,10);
+                document.getElementById("endDate").value=convertDate2;
+            
+                document.getElementById("numdays").value="7";
+            }       
+        //last 30 days 
+              else if(id === "last30days") {
+                let myNewDate= new Date(currDate);   
+                let finalDate=currDate.setDate(currDate.getDate() - 29);
+
+                let dateObject = new Date(finalDate);
+                let dateObject2 = new Date (myNewDate);
+
+                let convertDate=dateObject.toISOString().substr(0,10);
+                let convertDate2=dateObject2.toISOString().substr(0,10);
+
+                document.getElementById("startDate").value=convertDate;
+                document.getElementById("endDate").value=convertDate2;
+
+                document.getElementById("numdays").value="30";
+            }
+        //this month 
+              else if(id === "thismonth") {
+   
+                first_day_of_month = new Date(currDate.getFullYear(),currDate.getMonth(),+1); 
+                last_day_of_month = new Date(currDate.getFullYear(),currDate.getMonth() + 1,0); 
+            
+                let firstDay=first_day_of_month.setDate(first_day_of_month.getDate() + 1);
+                let LastDay=last_day_of_month.setDate(last_day_of_month.getDate() + 1);
+            
+                let dateObject = new Date(firstDay);
+                let convertDate=dateObject.toISOString().substr(0,10);
+                document.getElementById("startDate").value=convertDate;
+            
+                let dateObject2 = new Date(LastDay);
+                let convertDate2=dateObject2.toISOString().substr(0,10);
+                document.getElementById("endDate").value=convertDate2;
+                document.getElementById("numdays").value="30";
+            }
+        //this year 
+              else {
+                let currDate = new Date(document.getElementById("endDate").value);
+           
+                first_day_of_year = new Date(currDate.getFullYear(), 0); 
+                let firstDay=first_day_of_year.setDate(first_day_of_year.getDate()+1);
+                last_day_of_year = new Date(currDate.getFullYear(), 12)  
+                    
+                let dateObject = new Date(firstDay);
+                let convertDate=dateObject.toISOString().substr(0,10);
+                document.getElementById("startDate").value=convertDate;
+                
+                let dateObject2 = new Date(last_day_of_year);
+                let convertDate2=dateObject2.toISOString().substr(0,10);
+                document.getElementById("endDate").value=convertDate2;
+                document.getElementById("numdays").value="365";
+              }    
+      });
